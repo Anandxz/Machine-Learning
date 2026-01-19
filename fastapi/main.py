044de6de19322640cb7a2eddc144f46114e7096c
Anandxz <dubeyanand00000@gmail.com>
2026-01-19 22:19:23 +0530

    Http Extension

---
+++ fastapi/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI , Path ,HTTPException
+
+
+app = FastAPI()
+
+
+@app.get("/")
+
+def hello():
+    return {'message':"HEllo World this is your first API"}
+
+
+@app.get('/about')
+def about():
+    return {'message':"This is the seconf api "}
+
+def 
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id : str = Path(..., description ='Id of the patient',example = 'P100343')):
+    # loading all data
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404,detail="Patient Not found")
+            HTTPException()
+    
